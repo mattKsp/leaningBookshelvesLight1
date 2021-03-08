1f107e27a6a1b1ab5a86f0e97a2ccf12c1720d63
MTS Standish <16389869+mattThurstan@users.noreply.github.com>
2021-03-08 18:47:29 +0000

    v0,529 - turned serial back on + fix crash (see comms debug options)

--- Main/leaningBookShelvesLight1_Mesh/comms.ino
+++ Main/leaningBookShelvesLight1_Mesh/comms.ino
@@ -4,10 +4,14 @@
 void setupMesh() 
 {
   //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+  //mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+  //mesh.setDebugMsgTypes( CONNECTION | SYNC );
+  
+  // There is an issue with ESP32 crashing. It seesm to work if the following debug options are left active.
+  mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION | SYNC  );
 
   //mesh.init( MESH_NAME, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, STATION_CHANNEL );
-  mesh.init(MESH_NAME, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, STATION_CHANNEL, 1, 4); // hidden
+  mesh.init(MESH_NAME, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, STATION_CHANNEL, MESH_NODE_HIDDEN, MESH_NODE_MAX_CONN); // hidden
 
   mesh.setContainsRoot(true);
   mesh.onReceive(&receivedCallback);
@@ -15,5 +19,3 @@ void setupMesh()
   mesh.onChangedConnections(&changedConnectionCallback);
   mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
 }
-
-
